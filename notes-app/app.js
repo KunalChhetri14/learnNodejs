ac954311fe65d24ab55d3f793b031e85768a4044
Kunal Chhetri <kunalchhetri14@gmail.com>
2022-05-10 10:11:46 +0530

    feat(notes-app) : exporting file content

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
+const exportContent = require('./exportFile');
 
 fs.appendFile('notes2.txt', 'Appending contents to file', err => {
     if(err) throw err;
     console.log("file append succesffully");
 });
+
+console.log(exportContent.add());
+console.log("value is ", exportContent.a)
+
+
---
+++ notes-app/exportFile.js
@@ -0,0 +1,8 @@
+a = 40;
+b = 20;
+
+const add = function(a = 10, b = 20) {
+    return a + b;
+}
+
+module.exports = {add, a}

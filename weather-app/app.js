30e9412a2a388f59f166a0dd398b7d5a9577c878
Kunal Chhetri <kunalchhetri14@gmail.com>
2022-05-24 11:25:41 +0530

    feat(notes-app): mapbox api called

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,10 +3,19 @@ const constants = require('./Constants')
 
 console.log(constants);
 
-request(`http://api.weatherstack.com/current?access_key=${constants.apiKey}&query=%27New%20York%27`,
+const weatherTemp = request(`http://api.weatherstack.com/current?access_key=${constants.apiKey}&query=%27New%20York%27`,
     (error, response, body) => {
-    console.log(error);
-    console.log(response);
+    if(error) {
+        console.log(error);
+    }
+    const data = JSON.parse(response.body)
+    console.log(data);
+    console.log(`It is currently ${data.current.temperature} degrees out. There is `)
 });
+
+request('https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoia3VuYWxjaGhldHJpMTQiLCJhIjoiY2wzajI1d2M5MWloejNrcGdhODVpZ2lreiJ9.jDyOp9NgQqJkUm6O2ormzg',
+    (error, response, body) => {
+    console.log("the response is ", response)
+    })
 
 
